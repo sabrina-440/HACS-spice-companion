c895bcba7d7d3b7e29b95a51ac2c3cb35fee53a4
sabrina-440 <sabrinaxjp@gmail.com>
2026-07-08 17:29:52 -0700

    fix

--- custom_components/spice/entity.py
+++ custom_components/spice/entity.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.device_info import DeviceInfo
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 
 from .client import SpiceClient
